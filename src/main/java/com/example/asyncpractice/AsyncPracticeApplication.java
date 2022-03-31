59d22fd92eddc36b24a0292a1709214dc872d077
ksyoo.nextree <ksyoo@nextree.io>
2022-03-31 10:21:44 +0900

    Initial commit

---
+++ src/main/java/com/example/asyncpractice/AsyncPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.asyncpractice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsyncPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AsyncPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/asyncpractice/AsyncPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.asyncpractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsyncPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
